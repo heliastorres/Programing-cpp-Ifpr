4b30ab7e582700645d08a4b4946d4e6e06f20bb6
Helias Torres Oliveira <helias.torres12@gmail.com>
2025-04-10 13:07:17 -0300

    bee

---
+++ LISTAS ORLANDO (BeeCrowd)/LISTA 03 - Números Reais/1002 Área do Círculo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+int main () {            //ERROR CODE
+
+    int raio;
+    float area;
+    area = raio*raio*3.14159;
+
+    cin >> raio;
+    cout << "area=" << area << endl;
+
+    return 0;
+}
---
+++ LISTAS ORLANDO (BeeCrowd)/LISTA 03 - Números Reais/1005 Média 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+int main () {            
+
+    double A, B, media;
+
+    cin >> A >> B;
+    A = A * 3.5;
+    B = B * 7.5;
+
+    return 0;
+}
