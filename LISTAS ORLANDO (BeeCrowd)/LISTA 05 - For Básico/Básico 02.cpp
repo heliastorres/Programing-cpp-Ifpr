db09bd89f43d35f65806a7b0e43a66b9b2c23015
Helias Torres Oliveira <helias.torres12@gmail.com>
2025-05-02 17:58:53 -0300

    bee

---
+++ LISTAS ORLANDO (BeeCrowd)/LISTA 05 - For Básico/Básico 02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int valor, i;
+
+    cin >> valor;
+
+    for (i = 1; i <= valor; i++) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
---
+++ LISTAS ORLANDO (BeeCrowd)/LISTA 05 - For Básico/Básico 03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int inicio, final, i;
+
+    cin >> inicio >> final;
+
+    for (i = inicio; i <= final; i++) {
+        cout << i << endl;
+    }
+    
+
+    return 0;
+}
