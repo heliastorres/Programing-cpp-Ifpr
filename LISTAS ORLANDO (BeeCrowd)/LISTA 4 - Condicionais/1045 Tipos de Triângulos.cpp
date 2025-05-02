d9ba2583326b268b5381c80c4b0d611bdf9b7a87
Helias Torres Oliveira <helias.torres12@gmail.com>
2025-05-02 16:52:49 -0300

    bee

--- LISTAS ORLANDO (BeeCrowd)/LISTA 4 - Condicionais/1045 Tipos de Triângulos.cpp
+++ LISTAS ORLANDO (BeeCrowd)/LISTA 4 - Condicionais/1045 Tipos de Triângulos.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-main() {
+int main() {
     
-    cout << "ola" << endl;
+    double a, b, c;
+    double maior, meio, menor;
+
+    cin >> a >> b >> c;
+
+    if (a>b && a>c) {
+        maior = a;
+        meio = b;
+        menor = c;
+    }
+    else if (b>a && b>c) {
+        maior = b;
+        meio = a;
+        menor = c;
+    }
+    else {
+        maior = c;
+        meio = a;
+        menor = b;
+    }
+
+
+    if (maior < (meio+menor)) {
     
+                
+         if (pow(maior,2) == (pow(meio,2) + pow(menor,2))) {
+            cout << "TRIANGULO RETANGULO" << endl;
+        }
+        else if (pow(maior,2) > (pow(meio,2) + pow(menor,2))) {
+            cout << "TRIANGULO OBTUSANGULO" << endl;
+        }
+        else  if (pow(maior,2) < (pow(meio,2) + pow(menor,2))) {
+            cout << "TRIANGULO ACUTANGULO" << endl;
+        }
+            if (maior == meio && meio == menor) {
+                cout << "TRIANGULO EQUILATERO" << endl;
+            }
+            else if ((maior == meio && meio != menor) || (maior == menor && menor != meio) || (meio == menor && menor != maior)) {
+                cout << "TRIANGULO ISOSCELES" << endl;
+            }
+    }
+    else {
+        cout << "NAO FORMA TRIANGULO" << endl;
+    }
+    
+
     return 0;
 }
