4abb21c17dd1871335803385275e05360d5b9455
Helias Torres Oliveira <helias.torres12@gmail.com>
2025-05-15 12:30:12 -0300

    bee

---
+++ LISTAS ORLANDO (BeeCrowd)/LISTA 05 - For Básico/Básico 15.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int n, n2, i;
+
+    // cin >> n;
+
+    for (i = 1; i <= 10; i++) {
+        cin >> n;
+        n2 = n*2;
+        cout << n2 << endl;
+    }
+
+    return 0;
+}
