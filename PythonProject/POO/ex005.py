27ceeea4151c94c5d82859a803512d1f269a0eb3
Helias Torres Oliveira <helias.torres12@gmail.com>
2025-09-22 16:50:33 -0300

    Update ex005.py

--- PythonProject/POO/ex005.py
+++ PythonProject/POO/ex005.py
@@ -1,6 +1,14 @@
-def Carro:
-    def __init__(self, nome):
+class Pessoa:
+    def __init__ (self, nome):
         self.nome = nome
 
-    def acelerar(self):
-        print(f"{self.nome} está acelerando!")
+    def setNome(self, novoNome):
+        self.nome = novoNome
+
+    def getNome(self):
+        print(self.nome)
+
+p1 = Pessoa("João")
+p1.getNome ()
+p1.setNome("José")
+p1.getNome()
